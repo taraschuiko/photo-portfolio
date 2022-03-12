f07b14a67bc0f9ef521c1cb9d01458658268fe8d
Taras Chuiko <taraschuiko@gmail.com>
2022-03-12 11:12:10 +0200

    Added breakpoints, removed unused code

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Masonry from "react-masonry-css";
+import { useInView } from "react-intersection-observer";
 import "./index.css";
+import white from "../../assets/white.png";
 
 function importAll(r) {
   return r.keys().map(r);
 }
 
 const images = importAll(
-  require.context("../../photos", false, /\.(png|jpe?g|svg)$/)
-).sort((a, b) => 0.5 - Math.random());
+  require.context("../../photos", false, /\.(png|jpe?g)$/)
+).sort(() => 0.5 - Math.random());
+
+const Image = ({ image }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+  });
+
+  const [seen, setSeen] = useState(false);
+
+  useEffect(() => {
+    if (inView) {
+      setSeen(true);
+    }
+  }, [inView]);
+
+  return <img ref={ref} src={seen ? image : white} alt={image} />;
+};
+
+const breakpointColumns = {
+  default: 3,
+  768: 2,
+  425: 1
+};
 
 const Gallery = () => (
   <Masonry
-    breakpointCols={3}
+    breakpointCols={breakpointColumns}
     className="my-masonry-grid"
     columnClassName="my-masonry-grid_column"
   >
     {images.map((image) => (
-      <img src={image} key={image} alt={image} />
+      <Image image={image} key={image} />
     ))}
   </Masonry>
 );
