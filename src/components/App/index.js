8626b400c09aa067a2528d676f9a8c00835e1208
Taras Chuiko <taraschuiko@gmail.com>
2022-04-25 14:16:56 +0300

    Added social links

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,30 @@
 import "./index.css";
 import Gallery from "../Gallery";
 import me from "../../assets/me.jpg";
+import instagramIcon from "../../assets/icons/instagram.svg"
+import pexelsIcon from "../../assets/icons/pexels.svg"
+import unsplashIcon from "../../assets/icons/unsplash.svg"
 
 function App() {
   return (
     <div className="App">
-      <h1>Taras Chuiko</h1>
-      <span>Photography</span>
+      <header className="header">
+        <div>
+          <h1>Taras Chuiko</h1>
+          <span>Photography</span>
+        </div>
+        <div>
+          <a href="https://www.instagram.com/taraschuiko/" target="_blank" rel="noreferrer">
+            <img src={instagramIcon} alt="Instagram" />
+          </a>
+          <a href="https://www.pexels.com/@taraschuiko" target="_blank" rel="noreferrer">
+            <img src={pexelsIcon} alt="Pexels" />
+          </a>
+          <a href="https://unsplash.com/@taraschuiko" target="_blank" rel="noreferrer">
+            <img src={unsplashIcon} alt="Unsplash" />
+          </a>
+        </div>
+      </header>
       <img src={me} alt="Taras Chuiko" width="100%" />
       <Gallery />
     </div>
